95e38913cdc35899e53d82d9280703b7860ac717
Andrew Ang <73231496+SumDumPhuc@users.noreply.github.com>
2023-06-06 01:48:21 +0800

    Fixed Firebase.js issue

--- src/components/pages/Rules.js
+++ src/components/pages/Rules.js
@@ -104,7 +104,7 @@ function Rules() {
       width: '120px', 
       height: 'auto', 
     }}/> 
-    <p style={{ color: 'blue' }} className='image-message'>Winning</p>
+    <p style={{ color: 'blue' }} className='image-message'>EYE</p>
     </div>
 
     <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To win in Mahjong, you must have one EYE and 4 sets of any combination of CHOW's, PONG's, and KONG's. An example of a winning hand is shown below.</h3>
@@ -115,6 +115,39 @@ function Rules() {
       height: 'auto', 
     }}/> 
     </div>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>How to play</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>A game of mahjong is where tiles are distributed to the players and the game is played until a players declares Mahjong (GAME) or the unused tiles run out and the game is over (nobody wins). A round of mahjong is made up of many games. At the beginning of the mahjong round a banker is determined usually by rolling the dice. For the purposes of the computer game the computer user is chosen to be the initial banker. When a player has Mahjong (GAME) or the tiles run out and someone has declared a KONG, the banker position rotates to the player to the right (counterclockwise) of the banker of the previous game, otherwise the banker remains the same. A mahjong round is complete when the banker position has rotated around the table four times.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Prevailing Wind</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When the mahjong round starts the Prevailing Wind starts at the East position. When the banker position rotates one full time around the table the prevailing wind changes to South, then West, then finally North. After the North prevailing wind the round is complete.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Player Wind</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>During each game, a player is assigned a wind value. This Player Wind value is different from the Prevailing Wind. The banker always has the East Player Wind for that game, and the player to the right of the banker is the South Player Wind. The player to the right of the South Player Wind is the West player Wind, and the player to the right of the West Player Wind is the North player Wind.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Starting the Game</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>The 148 tiles are shuffled and each player selects 13 tiles to start the game. Each player starts with 13 tiles and these tiles are not seen by the other players. These are called the players concealed tiles. If a player has a tile(s) in the animal or flower suit they must put that tile up to show the rest of the players. These tiles remain up for the rest of the game and are referred to as the players exposed tiles. When a player exposes an animal or flower tile, they will take a replacement tile from opposite end of the remaining unused tiles. The player who is the banker for this game will start by taking another tile from the remaining unused tiles. At this point the banker has 14 tiles and must discard a tile. The banker will discard a tile which is then shown to the rest of the players.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Playing the Game</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded by a player, the other players have the option of using that tile when it is first discarded. The different options for using discarded tiles are described below. If no one wants to use that discarded tile, the player to the right of the player who discarded picks a new tile and adds it to his concealed tiles. After the player picks a new tile they will discard a tile.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Different Moves</h2> 
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>GAME MOVE</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded, any other player may decide that they would like to win the game using that tile. If more than one person can win the game using that discarded tile then the person closest to the right hand side of the player (counterclockwise from the player) who discarded the tile will have priority and win the game. Further, the GAME move has priority over the KONG, PONG, and CHOW moves. A player invokes this move by clicking the GAME button. A player may not use a discarded tile to GAME (win) if that same tile was discarded previously and was not used by that player after that players last move. A players last move is defined by any new tile, chow, pong, or kong that was performed by the player. A player may not GAME on a tile that they last discarded. If a player picks a new tile and wants to win the game, they may do so by clicking the GAME button. To win the game a player must have a valid Mahjong combination and have the minimum number of Fan to win the game. (more on Fans below).</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>KONG MOVE</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded and a player has 3 of the same tiles in the concealed tiles, the player may declare a KONG move by clicking the KONG button. When this happens, the 3 concealed tiles and the discarded tile is moved to the players exposed tiles and the player will pick a replacement tile from the opposite side of the remaining tiles.
+    </h3>
+
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>
+    When a new tile is picked by a player, and that player has 3 of the same tiles in the exposed tiles, that player may declare a KONG move by clicking the KONG button. When this happens the new picked tile in the concealed tiles is transferred to the exposed tiles and the player will pick a replacement tile. Note, when a player performs this type of Kong, the tile that is put in the exposed tiles is treated as a discard for that moment. Any other player can use that tile to win the game if they are able. This is called Robbing the Kong.
+    </h3>
+
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>
+    When a new tile is picked by a player, and there are 4 of the same tiles in that players concealed tiles, that player may declare a KONG move by clicking the KONG button. When this happens, the 4 concealed tiles used for the Kong are transferred to the exposed tiles and the player will pick a replacement tile. If a player invokes the KONG move and more than one KONG move is possible, the player must click on the tile that they wish to KONG.
+    </h3>  
+
 
 
 
