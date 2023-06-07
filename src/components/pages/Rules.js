24ad0db08b51c82a81b2d72fdc86ef214d2c4657
Andrew Ang <73231496+SumDumPhuc@users.noreply.github.com>
2023-06-07 15:15:48 +0800

    Rules Done

--- src/components/pages/Rules.js
+++ src/components/pages/Rules.js
@@ -10,6 +10,13 @@ import Pong from '../../images/Pong.jpg';
 import Kong from '../../images/Kong.jpg';
 import Eye from '../../images/Eye.jpg';
 import Win from '../../images/Win.jpg';
+import AllChow from '../../images/AllChow.jpg';
+import AllPong from '../../images/AllPong.jpg';
+import HalfColor from '../../images/HalfColor.jpg';
+import FullColor from '../../images/FullColor.jpg';
+import Half19 from '../../images/Half19.jpg';
+import Full19 from '../../images/Full19.jpg';
+import ThirteenWonders from '../../images/13Wonders.jpg';
 
 import './Rules.css';
 
@@ -136,6 +143,12 @@ function Rules() {
     <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>GAME MOVE</h2>
     <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded, any other player may decide that they would like to win the game using that tile. If more than one person can win the game using that discarded tile then the person closest to the right hand side of the player (counterclockwise from the player) who discarded the tile will have priority and win the game. Further, the GAME move has priority over the KONG, PONG, and CHOW moves. A player invokes this move by clicking the GAME button. A player may not use a discarded tile to GAME (win) if that same tile was discarded previously and was not used by that player after that players last move. A players last move is defined by any new tile, chow, pong, or kong that was performed by the player. A player may not GAME on a tile that they last discarded. If a player picks a new tile and wants to win the game, they may do so by clicking the GAME button. To win the game a player must have a valid Mahjong combination and have the minimum number of Fan to win the game. (more on Fans below).</h3>
 
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>CHOW MOVE</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded, the player to the right of the player who discarded may choose to CHOW the tile. When a player CHOW's a discarded tile, the discarded tile plus two tiles from the players concealed tiles are moved to that players exposed tiles. The GAME, KONG, and PONG moves have priority over the CHOW move. If the user decides to CHOW there may be multiple ways of obtaining the CHOW. The user should click on the tiles that they wish to use for the CHOW. A player may not CHOW on a tile that he last discarded.</h3>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>PONG MOVE</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded, any other player may decide that they would like to PONG using that tile. When a player PONG's a discarded tile, the discarded tile plus two tiles from the players concealed tiles are moved to that players exposed tiles. A player may not use a discarded tile to PONG if that same tile was discarded previously and was not used by that player after that players last move. A players last move is defined by any new tile, chow, pong, or kong that was performed by the player. A player may not PONG on a tile that they last discarded. The GAME and KONG moves have priority over the PONG move.</h3> 
+
     <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>KONG MOVE</h2>
     <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>When a tile is discarded and a player has 3 of the same tiles in the concealed tiles, the player may declare a KONG move by clicking the KONG button. When this happens, the 3 concealed tiles and the discarded tile is moved to the players exposed tiles and the player will pick a replacement tile from the opposite side of the remaining tiles.
     </h3>
@@ -148,8 +161,120 @@ function Rules() {
     When a new tile is picked by a player, and there are 4 of the same tiles in that players concealed tiles, that player may declare a KONG move by clicking the KONG button. When this happens, the 4 concealed tiles used for the Kong are transferred to the exposed tiles and the player will pick a replacement tile. If a player invokes the KONG move and more than one KONG move is possible, the player must click on the tile that they wish to KONG.
     </h3>  
 
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>NEXT MOVE</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>If a player does not want to use the discarded tile, the NEXT MOVE button is selected and play will proceed.
+    </h3> 
 
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>End of Game</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>The game is finished when someone wins the game or when there are 15 tiles left. When a player picks the last remaining new tile in play, it may be used to win the game or perform a Kong. It also can be used to complete an animal or flower set. However that player is not required to discard any tiles after the last tile has been taken. After the last tile has been taken a replacement tile for a Kong, Flower or Animal will not be taken. The computer game will ask for a discard (the player should click NEXT MOVE) in this situation, however the card is not actually discarded and the game is finished at that point.
+    </h3> 
 
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Types of Winning Hands</h2>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>All CHOW Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>In Singapore Mahjong the rules for All Chow game are very unique and different from other types of Mahjong. To have an All Chow game the following criteria must be meet;
+    </h3>   
+    <h3 style={{ marginTop: '5px', marginBottom: "5px"}}>1. The EYE cannot be a Dragon Tile or the Prevailing Wind or the Players Wind for that game.
+    </h3>   
+    <h3 style={{ marginTop: '5px', marginBottom: "5px"}}>2. The tiles must be made up of one EYE and four CHOWs.</h3>   
+    <h3 style={{ marginTop: '5px', marginBottom: "5px"}}>3. If the player wins the game on a discarded tile, there must be more than one unique tile that could have caused the player to win the game (get mahjong).
+    </h3>  
+
+    <div className='image-container'>
+    <img src={ AllChow } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Ping Hu Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>A Ping Wu game is an All Chow game with no tiles of the flower suit or animal suit in the players exposed tiles.
+    </h3>   
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>All PONG Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have an All Pong game the tiles must be made of an EYE and four PONG's and or KONG's.
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ AllPong } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Half Color Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have a Half Color game, the tiles must be made of the either the Bamboo suit, the Circle Suit, or Numbers Suit combined with tiles from the Honor Suit (Winds and Dragons).
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ HalfColor } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div>
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Full Color Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have a Full Color game, the tiles must be made of the either the Bamboo suit only, the Circle Suit only, the Numbers Suit only, or the Honor Suit only.
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ FullColor } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div>  
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Full One's and Nine's Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have an All one's and nine's game, all the tiles must be a combination of one's and nine's only.
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ Half19 } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div> 
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Half One's and Nine's Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have a Half one's and nine's game, all the tiles must be a combination of one's and nine's combined with the Honor Suit (Wind Suit and Dragon Suit).
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ Full19 } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div> 
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>13 Wonders Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>To have 13 Wonders you must have the one and the nine of the Bamboo suit, the Circle Suit, and the Numbers Suit along with each unique tile in the Honors suit. You must also have an EYE which can be made up of any of the above mentioned tiles.
+    </h3>    
+
+    <div className='image-container'>
+    <img src={ ThirteenWonders } alt="" style={{
+          width: '700px', 
+          height: 'auto', 
+        }} /> 
+    <p style={{ color: 'blue' }} className='image-message'></p>
+    </div>  
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Eight flower suit Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>In the case that a player has all eight Flower Suit tiles that person automatically wins the game regardless of their other tiles. If a player has seven Flower Suit tiles and another player picks the eighth and last Flower Suit tile, this eighth Flower Suit tile is automatically transferred to the player with seven Flower Suit tiles and that player automatically wins the game regardless of whether they have a valid mahjong combination.
+    </h3>    
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Da San Yuan Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>In the case where a player has a Pong or Kong of all the 3 dragons tiles they will automatically win the game regardless of whether they have a valid mahjong combination.
+    </h3>   
+
+    <h2 style={{ marginTop: '20px', textDecoration: 'underline' }}>Da Si Xi Hand</h2>
+    <h3 style={{ marginTop: '10px', marginBottom: "10px"}}>In the case where a player has a Pong or Kong of all the 4 wind tiles they will automatically win the game regardless of whether they have a valid mahjong combination.
+    </h3>   
 
     </div>
     </>
