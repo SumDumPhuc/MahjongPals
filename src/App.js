002a8917a4186a235f61dbf29a86ec41956433cb
Andrew Ang <73231496+SumDumPhuc@users.noreply.github.com>
2023-06-29 22:16:38 +0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-         <Route path='/' exact element={<Home/>} />
+         <Route path='/mahjongpals' exact element={<Home/>} />
          <Route path='/tutorial' element={<Tutorial/>} /> 
          <Route path='/rules' element={<Rules/>} />
          <Route path='/quiz' element={<Quiz/>} />
